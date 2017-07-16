e7852139178ba5e1318e1045b3a3f4de53a38ec8
Chase Eaton <chasegeaton@gmail.com>
2017-07-16 09:30:15 -0500

    Fixed Meteor methods

--- server/main.js
+++ server/main.js
@@ -3,28 +3,30 @@ const homeaway = require('./homeaway.js');
 const expedia = require('./expedia.js');
 
 Meteor.startup(() => {
-    Meteor.methods({
-	getCurrentTime: function () {
-	    console.log('on server, getCurrentTime called');
-	    return new Date();
-	},
+    console.log('server started');
+});
 
-	getActivities: function(func) {
-	    console.log('getting activities');
-	    var promise = new Promise((resolve, reject) => {
-		get_activities('Austin', 'Texas',
-			       '2017-7-10', '2017-7-13', resolve);
-	    })
-	    return promise.await();
-	},
+Meteor.methods({
+    getCurrentTime: function () {
+	console.log('on server, getCurrentTime called');
+	return new Date();
+    },
 
-	getHomes: function(func) {
-	    console.log('getting homes');
-	    var promise = new Promise((resolve, reject) => {
-		get_homes('Austin', 'Texas', '2017-7-10', '2017-7-13',
-			  null, null, null, resolve);
-	    })
-	    return promise.await();
-	}
-    });
+    getActivities: function(func) {
+	console.log('getting activities');
+	var promise = new Promise((resolve, reject) => {
+	    get_activities('Austin', 'Texas',
+			   '2017-7-10', '2017-7-13', resolve);
+	});
+	return promise.await();
+    },
+
+    getHomes: function(func) {
+	console.log('getting homes');
+	var promise = new Promise((resolve, reject) => {
+	    get_homes('Austin', 'Texas', '2017-7-10', '2017-7-13',
+		      null, null, null, resolve);
+	});
+	return promise.await();
+    }
 });
